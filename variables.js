0a3c6565d831d0112c2396699a9e09f2a13aa1e1
Boikano11 <pooetsholofetso@gmail.com>
2025-04-11 05:57:09 +0200

    Variables

---
+++ variables.js
@@ -0,0 +1,37 @@
+// Variable Declaration
+var myName = "Tsholofetso";
+var age = 31;
+var isStudent = true;
+
+console.log(`
+Variable Declaration:
+Name:${myName}
+Age: ${age}
+Student: ${isStudent}`);
+
+// Variable Reassignment
+var favoriteColor = "blue";
+console.log(`
+Variable Reassignment:
+Favorite color: ${favoriteColor}
+`);
+
+favoriteColor = "black";
+console.log(`Favorite color: ${favoriteColor}`);
+
+// Arithmetic Operators
+var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+
+console.log(`
+Arithmentic Operators:
+ Operands: ${num1}, ${num2}
+ Sum: ${sum}
+ Difference: ${difference}
+ Product: ${product}
+ Quotient: ${quotient}    
+`);
